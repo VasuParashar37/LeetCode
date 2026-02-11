01e4b1e066d6b58360416195f39c7c7bbe3d19a6
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-02-11 10:00:53 +0530

    Time: 6 ms (86.73%), Space: 44.1 MB (75.84%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        if (n > m) return false;
-        for (int i = 0; i <= m - n; i++) {
-            // Get substring of length n
-            String sub = s2.substring(i, i + n);
-            if (isPermutation(s1, sub)) {
-                return true;
+        if (s1.length() > s2.length()) return false;
+        int[] freq = new int[26];
+        // Count characters of s1
+        for (char c : s1.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        int left = 0;
+        int right = 0;
+        int required = s1.length();
+        while (right < s2.length()) {
+            char ch = s2.charAt(right);
+            // If this character is useful, reduce required count
+            if (freq[ch - 'a'] > 0) {
+                required--;
+            }
+            // Decrease frequency (even if negative)
+            freq[ch - 'a']--;
+            right++;
+            // When window size equals s1 length
+            if (right - left == s1.length()) {
+                // If all characters matched
+                if (required == 0) {
+                    return true;
+                }
+                // Remove left character from window
+                char leftChar = s2.charAt(left);
+                if (freq[leftChar - 'a'] >= 0) {
+                    required++;
+                }
+                freq[leftChar - 'a']++;
+                left++;
             }
         }
         return false;
-    }
-    private boolean isPermutation(String a, String b) {
-        int[] freq = new int[26];
-        for (char c : a.toCharArray()) {
-            freq[c - 'a']++;
-        }
-        for (char c : b.toCharArray()) {
-            freq[c - 'a']--;
-        }
-        for (int count : freq) {
-            if (count != 0) return false;
-        }
-        return true;
     }
 }
