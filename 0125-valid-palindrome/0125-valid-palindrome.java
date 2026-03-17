630878e8da574986d43ccefcddda937fb60bb287
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-03-17 13:00:13 +0530

    Time: 3 ms (61.33%), Space: 44.7 MB (51.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //this is the second solution
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        System.out.println(s);
-        int i=0;
-        while(i<s.length()/2){
-            if(s.charAt(i)!=(s.charAt(s.length()-1-i))) return false;
-            i++;
+        int i=0;int j=s.length()-1;
+        s = s.toLowerCase();
+        while(i<j){
+            char ch = s.charAt(i);
+            char chr = s.charAt(j);
+            while(i<j && !Character.isLetterOrDigit(ch)){
+                i++;
+                ch = s.charAt(i);
+            }
+            while(i<j && !Character.isLetterOrDigit(chr)){
+                j--;
+                chr = s.charAt(j);
+            }
+            if(ch == chr){
+                i++;j--;
+            }
+            else return false;
         }
         return true;
     }
 }
+// a man, a plan, a canal: panama
