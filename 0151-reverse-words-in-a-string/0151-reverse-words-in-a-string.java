1228fd0074abbca7253b2afa4cfd1c20dab74da6
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-03-21 00:10:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public String reverseWords(String s) {
-        // s = s.trim();
         String[]arr = s.split(" ");
         System.out.println(Arrays.toString(arr));
         int i=0; int j=arr.length-1;
