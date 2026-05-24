e2793970d901698b1799be9f1c84b62bd2172759
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-05-24 23:56:49 +0530

    Time: 4 ms (16.75%), Space: 47.1 MB (9.98%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.java
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.java
@@ -4,22 +4,8 @@ class Solution {
         List<Integer>list=new ArrayList<>();
         for(int i:nums){
             map.put(i, map.getOrDefault(i,0)+1);
-        }
-        
-        int count = 0;
-        Iterator<Map.Entry<Integer, Integer>>it=map.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry<Integer, Integer>entry=it.next();
-            int key = entry.getKey();
-            int value = entry.getValue();
-            count=0;
-            for(int i=0;i<value;i++){
-                list.add(entry.getKey());
-                count++;
-                if(count==k){
-                    it.remove();
-                    break;
-                }
+            if(map.get(i)<=k){
+                list.add(i);
             }
         }
         int[]arr=new int[list.size()];
