3e959f8a660815c5e96ad8daa1d35763c900b6fd
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-06-08 22:16:48 +0530

    Time: 11 ms (28.37%), Space: 155.9 MB (90.51%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -8,21 +8,16 @@ class Solution {
             else if(i>pivot)
                 list2.add(i);
         }
-        System.out.println(list1);
-        System.out.println(list2);
         int equal = nums.length - (list1.size() + list2.size());
-        System.out.println(equal);
         int i=0;
         while(i<list1.size()){
             nums[i]=list1.get(i);
             i++;
         }
-        System.out.println(i);
         while(equal-->0){
             nums[i]=pivot;
             i++;
         }
-        System.out.println(i);
         int j=0;
         while(j<list2.size()){
             nums[i]=list2.get(j);
