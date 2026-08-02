bfe7146b8039419878e9ee0aac836dac78c669d7
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-08-02 22:58:31 +0530

    Time: 0 ms (100%), Space: 43.2 MB (35.84%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
+/**
+    1. What is my state?
+    2. What are my choices?
+    3. What changes after my move?
+    4. Whose turn is next?
+    5. Opponent is stupid?
+ */ 
