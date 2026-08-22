8e73b7b0a941fc8f876c444c33f29664148975c2
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-08-23 00:49:02 +0530

    Time: 1 ms (95.68%), Space: 43.6 MB (5.2%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int lastFoundIndex = -1;
-        for(int i=0;i<s.length();i++){
-            boolean isPresent = false;
-            for(int j=lastFoundIndex+1;j<t.length();j++){
-                if(s.charAt(i)==t.charAt(j)){
-                    if(j<lastFoundIndex)
-                        return false;
-                    lastFoundIndex = j;
-                    isPresent = true;
-                    break;
-                }
-            }
-            if(!isPresent)return false;
-        }
-        return true;
+        return recurse(0, 0, s, t);
+    }
+
+    private boolean recurse(int i, int j, String s, String t) {
+        if (i == s.length())
+            return true;
+        if (j == t.length())
+            return false;
+        if (s.charAt(i) == t.charAt(j))
+            i++;
+        j++;
+        return recurse(i, j, s, t);
     }
 }
