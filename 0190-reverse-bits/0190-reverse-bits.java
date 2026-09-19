8fca5def8b8e2b93628f3dbc6cb9eafdfb5de744
Vasu Parashar <123638048+VasuParashar37@users.noreply.github.com>
2026-09-19 19:29:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (18.59%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
         for (int i = 0; i < 32; i++) {
             int bit = n & 1;
-            n = n >>>= 1;
+            n = n >> 1;
             ans = ans << 1 | bit;
         }
         return ans;
